29eb17c5e9d33718a1bd17054be8609d501de917
alexbponder@gmail.com <alexbponder@gmail.com>
2021-12-02 18:25:21 -0800

    get using axios

--- server.js
+++ server.js
@@ -1,11 +1,30 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080;
+require('dotenv').config();
+const mysql = require('mysql2');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+// create the connection to database
+const connection = mysql.createConnection({
+  host:process.env.host,
+  user:process.env.user,
+  database:process.env.database,
+  password:process.env.password
+});
+
+app.get('/api', (req, res) => {
+  // simple query
+connection.query(
+  'SELECT * FROM t_list_locations;',
+  function(err, results, fields) {
+    console.log(results); // results contains rows returned by server
+    console.log(fields); // fields contains extra meta data about results, if available
+    res.json(results)
+  }
+);
+  // res.send('Hello World!')
+
 })
 
 app.listen(PORT, () => console.log(`server listening on ${PORT}`));
-
 
