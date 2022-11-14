cd3434257e6914856000c35e17bbe331f19f3d59
alexbponder@gmail.com <alexbponder@gmail.com>
2022-11-14 14:15:48 -0800

    ddd

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080;
 // require('dotenv').config();
+//more notes
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
